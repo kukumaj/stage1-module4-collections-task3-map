04f558c7562769898c11f1e8e1f66f678db9ef6d
kukumaj <zakumaj@gmail.com>
2022-10-18 09:47:34 +0200

    done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,28 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()
+        ) {
+            if (map.containsKey(entry.getKey())) {
+                for (Map.Entry<String, Integer> entry2 : map.entrySet()
+                ) {
+                    if (entry.getValue().equals(entry2.getKey())) {
+                        if (entry.getKey() <= entry2.getValue()) {
+                            map.put(entry.getValue(), entry2.getValue());
+
+                        } else {
+                            map.put(entry.getValue(), entry.getKey());
+                        }
+                    }
+                }
+            } else {
+                map.put(entry.getValue(), entry.getKey());
+            }
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
-
+import java.util.HashMap;
 import java.util.Map;
-
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        sentence = sentence.toLowerCase();
+        String[] s = sentence.replaceAll("[,.]", "").split(" ");
+        for (String element : s
+        ) {
+            if (!element.isEmpty()) {
+                int count = 1;
+                if (map.get(element) != null) {
+                    count = map.get(element) + 1;
+                    map.put(element, count);
+                } else {
+                    map.put(element, count);
+                }
+            }
+        }
+        return map;
     }
 }
