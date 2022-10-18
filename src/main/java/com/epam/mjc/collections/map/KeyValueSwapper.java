5493332620ccc1a60b6f2db7b1e417583c71b7fb
kukumaj <zakumaj@gmail.com>
2022-10-18 09:54:54 +0200

    code improvement

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -5,22 +5,20 @@ import java.util.*;
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> map = new HashMap<>();
-        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()
-        ) {
-            if (map.containsKey(entry.getKey())) {
-                for (Map.Entry<String, Integer> entry2 : map.entrySet()
-                ) {
-                    if (entry.getValue().equals(entry2.getKey())) {
-                        if (entry.getKey() <= entry2.getValue()) {
-                            map.put(entry.getValue(), entry2.getValue());
+        for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
 
+            if (map.containsKey(e.getValue())) {
+                for (Map.Entry<String, Integer> e2 : map.entrySet()) {
+                    if (e2.getKey().equals(e.getValue())) {
+                        if (e.getKey() >= e2.getValue()) {
+                            map.put(e.getValue(), e2.getValue());
                         } else {
-                            map.put(entry.getValue(), entry.getKey());
+                            map.put(e.getValue(), e.getKey());
                         }
                     }
                 }
             } else {
-                map.put(entry.getValue(), entry.getKey());
+                map.put(e.getValue(), e.getKey());
             }
         }
         return map;
